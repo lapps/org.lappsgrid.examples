83d3d2e506a9eb6f556a522f1b42d27f2c430352
Di Wang <diwang@cs.cmu.edu>
2013-04-30 11:19:43 -0400

    Added new DataSourceAPI folder

---
+++ DataSourceAPI/src/main/java/org/lapps/datasource/Query.java
@@ -0,0 +1,14 @@
+package org.lapps.datasource;
+
+/**
+ * Queries over {@link DataSource} to select matching documents.
+ * Possible queries include:
+ * 1) IR query for lucene/indri e.g. "(president AND Obama)";
+ * 2) regular expression matching query e.g. "(president .* Obama)" over text field and/or annotation field
+ * 3) composite query built from several queries e.g. "(query1 OR !query2)"
+ *
+ * @author Di Wang
+ */
+public abstract class Query {
+  
+}
