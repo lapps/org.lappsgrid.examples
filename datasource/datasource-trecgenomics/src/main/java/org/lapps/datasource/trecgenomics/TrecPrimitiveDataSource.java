45c76ecdc0af2bd985b8742119b80f9be986eac7
Di Wang <diwang@cs.cmu.edu>
2013-05-30 16:55:14 -0400

    cleaned up pom

--- datasource/datasource-trecgenomics/src/main/java/org/lapps/datasource/trecgenomics/TrecPrimitiveDataSource.java
+++ datasource/datasource-trecgenomics/src/main/java/org/lapps/datasource/trecgenomics/TrecPrimitiveDataSource.java
@@ -9,13 +9,14 @@ import org.lapps.datasource.PrimitiveDataSource;
 
 /**
  * A fake data source mimic trec genomic dataset
- *
+ * 
  * @author Di Wang
  */
 @WebService
 public class TrecPrimitiveDataSource implements PrimitiveDataSource {
 
   @WebMethod
+  @Override
   public Data query(DataQuery query) {
 
     if (!query.getDiscriminator().equals("QUERY")) {
@@ -26,24 +27,23 @@ public class TrecPrimitiveDataSource implements PrimitiveDataSource {
     if (queryWords.length < 2 || !queryWords[0].toLowerCase().equals("select")) {
       throw new IllegalArgumentException("Query format is not supported.");
     }
-    
-    if(queryWords.length == 2){
+
+    if (queryWords.length == 2) {
       Data result = new Data();
       result.setDiscriminator("DOCUMENT");
-      result.setPayload("DOCUMENT No."+queryWords[1]);
+      result.setPayload("DOCUMENT No." + queryWords[1]);
       return result;
-    }else{
+    } else {
       Data result = new Data();
       result.setDiscriminator("IDs");
       StringBuffer payloadBuf = new StringBuffer();
       for (int i = 1; i < queryWords.length; i++) {
         int docId = Integer.parseInt(queryWords[i]);
-        payloadBuf.append(docId*13).append(" ");
+        payloadBuf.append(docId * 13).append(" ");
       }
       result.setPayload(payloadBuf.toString());
       return result;
     }
-    
   }
 
 }
