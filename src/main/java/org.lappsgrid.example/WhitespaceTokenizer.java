76d1b5698cb8d28f5b7e1ce694aabc4297e4ff0a
Keith Suderman <suderman@anc.org>
2015-06-13 18:59:08 -0400

    Started code update.

--- src/main/java/org.lappsgrid.example/WhitespaceTokenizer.java
+++ src/main/java/org.lappsgrid.example/WhitespaceTokenizer.java
@@ -1,120 +1,67 @@
 package org.lappsgrid.example;
 
-import org.anc.lapps.serialization.Annotation;
-import org.anc.lapps.serialization.Container;
-import org.anc.lapps.serialization.ProcessingStep;
-import org.lappsgrid.api.Data;
 import org.lappsgrid.core.DataFactory;
 import org.lappsgrid.discriminator.DiscriminatorRegistry;
-import org.lappsgrid.discriminator.Types;
+import static org.lappsgrid.discriminator.Discriminators.Uri;
+import org.lappsgrid.serialization.*;
+import org.lappsgrid.serialization.lif.*;
+
 import org.lappsgrid.api.WebService;
 
-public class WhitespaceTokenizer implements WebService {
-    public static final  String VERSION = "0.0.1-SNAPSHOT";
+import java.util.Map;
 
-    public WhitespaceTokenizer(){
-    }
+public class WhitespaceTokenizer implements WebService
+{
 
-    @Override
-    public long[] requires() {
-        return new long[]{};
-    }
+	public WhitespaceTokenizer()
+	{
+	}
 
-    @Override
-    public long[] produces() {
-        return new long[]{};
-    }
+	@Override
+	public String execute(String json)
+	{
+		Data data = Serializer.parse(json, Data.class);
+		if (is(data, Uri.ERROR))
+		{
+			// Return errors unmodified.
+			return json;
+		}
 
-    @Override
-    public Data execute(Data data) {
-        long discriminator = data.getDiscriminator();
-        if (discriminator == Types.ERROR)
-        {
-            return data;
-        }
-        else  if (discriminator == Types.TEXT) {
-            String text = data.getPayload();
-            Container container = new Container(false);
-            container.setText(text);
-            container.setLanguage("en");
-            ProcessingStep processingStep = container.newStep();
-            processingStep.addContains("Token", this.getClass().getName() + ":" + VERSION, "annotation:tokenizer");
+		Container container;
+		if (is(data, Uri.LAPPS))
+		{
+			container = new Container((Map) data.getPayload());
+		}
+		else if (is(data, Uri.TEXT))
+		{
+			container = new Container();
+			container.setText(data.getPayload().toString());
+			container.setLanguage("en");
+		}
+		else
+		{
+			String message = String.format("Unsupported discriminator type: %s", data.getDiscriminator());
+			return DataFactory.error(message);
+		}
 
-            int start = 0;
-            int end = 0;
-            for (int i = 0; i < text.length(); i++) {
-                char ch = text.charAt(i);
-                // is digital or letter
-                if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
-                    end ++;
-                    if (end == text.length()) {
-                        Annotation ann = processingStep.newAnnotation("Token", start, end);
-                        ann.addFeature("string", text.substring(start, end));
-                    }
-                } else {
-                    if (end > start) {
-                        Annotation ann = processingStep.newAnnotation("Token", start, end);
-                        ann.addFeature("string", text.substring(start, end));
-                    }
-                    start = i + 1;
-                    end = start;
-                    if(ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' || ch == '\f') {
-                        // white space.
-                    } else {  // punctuation
-                        Annotation ann = processingStep.newAnnotation("Token", i, i + 1);
-                        ann.addFeature("string", text.substring(i, i + 1));
-                    }
-                }
-            }
-            return DataFactory.json(container.toString());
+		data = new DataContainer(container);
+		return data.asJson();
+	}
 
-        } else  if (discriminator == Types.JSON) {
-            String textjson = data.getPayload();
+	@Override
+	public String getMetadata()
+	{
+		return null;
+	}
 
 
-            Container container = new Container(textjson);
-            container.setLanguage("en");
-            ProcessingStep processingStep = container.newStep();
-            processingStep.addContains("Token", this.getClass().getName() + ":" + VERSION, "annotation:tokenizer");
-            String text = container.getText();
+	protected void tokenize(Container container)
+	{
 
-            int start = 0;
-            int end = 0;
-            for (int i = 0; i < text.length(); i++) {
-                char ch = text.charAt(i);
-                // is digital or letter
-                if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
-                    end ++;
-                    if (end == text.length()) {
-                        Annotation ann = processingStep.newAnnotation("Token", start, end);
-                        ann.addFeature("string", text.substring(start, end));
-                    }
-                } else {
-                    if (end > start) {
-                        Annotation ann = processingStep.newAnnotation("Token", start, end);
-                        ann.addFeature("string", text.substring(start, end));
-                    }
-                    start = i + 1;
-                    end = start;
-                    if(ch == ' ' || ch == '\t' || ch == '\n' || ch == '\r' || ch == '\f') {
-                        // white space.
-                    } else {  // punctuation
-                        Annotation ann = processingStep.newAnnotation("Token", i, i + 1);
-                        ann.addFeature("string", text.substring(i, i + 1));
-                    }
-                }
-            }
-            return DataFactory.json(container.toString());
-        } else {
-            String name = DiscriminatorRegistry.get(discriminator);
-            String message = "Invalid input type. Expected Text but found " + name;
-            return DataFactory.error(message);
-        }
-    }
-
-    public Data configure(Data config) {
-        return null;
-    }
-
-
+	}
+	
+	private boolean is(Data<? extends Object> data, String type)
+	{
+		return type.equals(data.getDiscriminator());
+	}
 }
