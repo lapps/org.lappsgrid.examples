0da5f21a9c67d6577a1523d4faee638590f702ce
Keigh Rim <keigh.rim@gmail.com>
2015-07-14 00:03:43 -0400

    Removed old examples

--- masc/org.anc.grid.data.masc.client/src/main/java/org/anc/grid/data/masc/client/serial/package-info.java
+++
@@ -1,7 +0,0 @@
-/**
- * The classes in this package are not used. However, they will
- * serve as the starting points for custom serializers and 
- * deserializers.
- */
-
-package org.anc.grid.data.masc.client.serial;
